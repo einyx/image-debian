748376563f2088ae65260cbca2f8b18bd4c92dad
root <root@fdff6fbb9850>
2018-11-19 21:53:57 +0000

    Add Debian Buster

---
+++ buster/env.mk
@@ -0,0 +1,9 @@
+IMAGE_NAME = debian
+IMAGE_VERSION = buster
+IMAGE_VERSION_ALIASES = 10.0 10 testing 
+IMAGE_TITLE = Debian Buster
+IMAGE_DESCRIPTION = Debian Buster
+IMAGE_SOURCE_URL = https://github.com/scaleway/image-debian
+IMAGE_VENDOR_URL = https://www.debian.org
+IMAGE_BOOTSCRIPT = mainline 4.9
+IMAGE_BASE_FLAVORS = common docker-based systemd feature-motd
